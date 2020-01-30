f3a5f68668cce789ebf1541687ba54e5cdcc460d
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-01-30 15:19:15 +0200

    added the data models and tests

---
+++ models/__tests__/categories.test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const Categories = require('../categories/categories.js');
+
+describe('Categories', () => {
+
+  let categories;
+
+  beforeEach(() => {
+    categories = new Categories();
+  });
+
+  it('Post() a new categories', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      })
+      .catch(e => console.error('ERR', e));
+  });
+
+  it('get() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.get(record._id)
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[0][key]).toEqual(obj[key]);
+            });
+          });
+      });
+  });
+  it('update() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        record.name = 'Test for update category';
+        return categories.update(record._id,record)
+          .then(category => {
+            return categories.get(category._id)
+              .then(category=>{
+              // console.log(category)
+                Object.keys(obj).forEach(key => {
+                  expect(category[0][key]).toEqual(obj[key]);
+                });
+              });
+          });
+      });
+  });
+  it('delete() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.get(record._id)
+          .then(category => {
+            return categories.delete(category._id)
+              .then(category=>{
+              // console.log(category)
+                expect(category).toEqual(undefined);
+              });
+          });
+      });
+  });
+});
